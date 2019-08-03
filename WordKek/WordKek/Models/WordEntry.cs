e4be3b16cbed5c04f75be5837414d925052e0e1e
Hryhorii Pazych <supersoild2000@outlook.com>
2019-08-03 21:54:53 +0300

    Added WordEntry and WordList

---
+++ WordKek/WordKek/Models/WordEntry.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WordKek.Models
+{
+
+    class WordEntry
+    {
+        public string Word { get; private set; }
+        public string Translation { get; private set; }
+        public bool IsLearned { get; private set; }
+        public ushort CorrectRepeatsCount { get; private set; }
+        /// <summary>
+        /// Parameterized constructor 
+        /// </summary>
+        /// <param name="word"> Foreign word</param>
+        /// <param name="translation"> Origin word</param>
+        public WordEntry(string word, string translation)
+        {
+            Word = word;
+            Translation = translation;
+            IsLearned = false;
+            CorrectRepeatsCount = 0;
+        }
+
+        public void IncreaseCorrectRepeatsCount(ushort maxRepeats)
+        {
+            if (CorrectRepeatsCount < maxRepeats)
+                CorrectRepeatsCount++;
+            else
+                IsLearned = true;
+        }
+        private void ResetCorrectRepeatsCount()
+        {
+            CorrectRepeatsCount = 0;
+        }
+        public bool Equals(WordEntry word)
+        {
+            return Word == word.Word && Translation == word.Translation;
+        }
+    }
+}
---
+++ WordKek/WordKek/Models/WordList.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WordKek.Models
+{
+    [Serializable]
+    class WordList
+    {
+        private List<WordEntry> dictionary;
+
+        public WordList()
+        {
+            dictionary = new List<WordEntry>();
+        }
+
+        public bool Add(WordEntry word)
+        {
+            if (!Contains(word))
+            {
+                dictionary.Add(word);
+                return true;
+            }
+            else
+                return false;
+        }
+        public bool Contains(WordEntry word)
+        {
+            return dictionary.Contains(word);
+        }
+
+    }
+}
