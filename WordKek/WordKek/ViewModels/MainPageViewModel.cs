b3dcb48baa68106b3f1207ec0aa63c79b46e0793
d4ywasted <d4ywasted@gmail.com>
2019-08-05 14:15:17 +0300

    Test2

--- WordKek/WordKek/ViewModels/MainPageViewModel.cs
+++ WordKek/WordKek/ViewModels/MainPageViewModel.cs
@@ -9,26 +9,9 @@ namespace WordKek.ViewModels
     {
         private MainWordList mainWordList;
 
-        private ClickModel clickModel;
-
-        public ICommand ClickCommand { get; private set; }
-
-        public string LabelText {
-            get => clickModel.Status;
-        }
-
         public MainPageViewModel()
         {
             mainWordList = FileHandler.OpenMainWordList();
-
-            ClickCommand = new Command(OnButtonClick);
-            clickModel = new ClickModel();
-        }
-
-        private void OnButtonClick()
-        {
-            clickModel.OnClick();
-            NotifyPropertyChanged(nameof(LabelText));
         }
     }
 }
